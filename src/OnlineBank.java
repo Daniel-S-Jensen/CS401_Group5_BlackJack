68f8b50681b2655eb9e656edadcad987ccf00b28
cyLee1991 <89447422+cyLee1991@users.noreply.github.com>
2022-07-22 00:43:12 -0700

    Update OnlineBank.java

--- src/OnlineBank.java
+++ src/OnlineBank.java
@@ -24,3 +24,42 @@ public class OnlineBank {
 	}
 	
 }
+/**
+import java.util.Scanner;  
+
+public class OnlineBank extends Player {
+	 Scanner scan = new Scanner(System.in); 
+	 
+
+	//attributes
+	
+	//Im thinking this onlinebank come after login page, therefore we dont need any login
+	public  login() {
+		
+	}
+	
+	//
+	public void addCash() {
+		
+		System.out.println("Enter the amount(interger) that you want to add ?");
+		int amount = scan.nextInt();
+		setAccountBalance(getAccountBalance() + amount);
+		System.out.println("Added succussfully, the current balance is $" + getAccountBalance());
+	}
+	
+	//
+	public void withdrawCash() {
+		
+		System.out.println("Enter the amount(interger) that you want to withdraw ?");
+		int amount = scan.nextInt();  
+		
+		if (getAccountBalance() >= amount) {  
+			setAccountBalance(getAccountBalance() - amount);
+			System.out.println("Withdraw succussfully, the current balance is $ " + getAccountBalance());  
+		} else {  
+			System.out.println("Your balance is not enough" + "\n Please try again !" );  
+		}  
+	}
+}
+
+*/
